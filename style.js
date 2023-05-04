30cd40e77d88aabd9df819aab979bb244625e5b9
Innovider <42913925+Innovider@users.noreply.github.com>
2023-05-04 09:16:21 +0600

    One

---
+++ style.js
@@ -0,0 +1,11 @@
+// https://api.whatsapp.com/send?phone=77772038035
+
+let link = 'https://api.whatsapp.com/send?phone=';
+
+$('button').click(function(){
+    let field = $('input').val();
+    let fField = field.replace('(','').replace(')','').replace('+','').replace(/ /g,'');
+    
+    $(location).attr('href',link + fField);
+
+})
