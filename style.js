f4ea42a17d2d42ff59f7654b78d30685b37e38f5
Innovider <42913925+Innovider@users.noreply.github.com>
2023-10-25 17:06:34 +0600

    delete some symbols Update style.js

--- style.js
+++ style.js
@@ -4,8 +4,39 @@ let link = 'https://api.whatsapp.com/send?phone=';
 
 $('button').click(function(){
     let field = $('input').val();
-    let fField = field.replace('(','').replace(')','').replace('+','').replace('-','').replace(/ /g,'');
+
+    for(let i = 0; i < field.length; i++){
+        if (i === 0 && field[i] === "8") {
+            link = link + 7;
+        }else if (
+          field[i] === "+" ||
+          field[i] === "(" ||
+          field[i] === ")" ||
+          field[i] === "-" ||
+          field[i] === " "
+        ){
+          link = links + "";  
+        }else{
+            link = link + field[i];
+        }   
+    }
     
-    $(location).attr('href',link + fField);
+    $(location).attr('href',link);
 
 })
+
+for (let i = 0; i < cNum.length; i++) {
+    if (i === 0 && cNum[i] === "8") {
+      fNum = fNum + 7;
+    } else if (
+      cNum[i] === "+" ||
+      cNum[i] === "(" ||
+      cNum[i] === ")" ||
+      cNum[i] === "-" ||
+      cNum[i] === " "
+    ) {
+      fNum = fNum + "";
+    } else {
+      fNum = fNum + cNum[i];
+    }
+  }
