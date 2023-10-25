740c8e825281042de7038b208de2c98e58ab3ab2
Innovider <42913925+Innovider@users.noreply.github.com>
2023-10-25 15:31:15 +0600

    - Update style.js

--- style.js
+++ style.js
@@ -4,7 +4,7 @@ let link = 'https://api.whatsapp.com/send?phone=';
 
 $('button').click(function(){
     let field = $('input').val();
-    let fField = field.replace('(','').replace(')','').replace('+','').replace(/ /g,'');
+    let fField = field.replace('(','').replace(')','').replace('+','').replace('-','').replace(/ /g,'');
     
     $(location).attr('href',link + fField);
 
