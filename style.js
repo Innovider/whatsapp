cc4ace425f9ba0ba8360e3936f3347f8a3db8dae
Innovider <42913925+Innovider@users.noreply.github.com>
2023-10-29 13:59:46 +0600

    fix errors Update style.js

--- style.js
+++ style.js
@@ -1,26 +1,25 @@
-// https://api.whatsapp.com/send?phone=77772038035
-
-let link = 'https://api.whatsapp.com/send?phone=';
-
-$('button').click(function(){
-    let field = $('input').val();
-
-    for(let i = 0; i < field.length; i++){
-        if (i === 0) {
-            link = `${link}7`;
-        }else if (
-              field[i] === "+" ||
-              field[i] === "(" ||
-              field[i] === ")" ||
-              field[i] === "-" ||
-              field[i] === " "
-            ){
-              link = link + "";  
-            }else{
-              link = link + field[i];
-            }
+document.getElementById("button").addEventListener("click", () => {
+  let number = document.getElementById("number");
+  let cNum = number.value;
+  let fNum = "";
+  for (let i = 0; i < cNum.length; i++) {
+    if (i === 0 && cNum[i] === "8") {
+      fNum = fNum + 7;
+    } else if (
+      cNum[i] === "+" ||
+      cNum[i] === "(" ||
+      cNum[i] === ")" ||
+      cNum[i] === "-" ||
+      cNum[i] === " "
+    ) {
+      fNum = fNum + "";
+    } else {
+      fNum = fNum + cNum[i];
     }
-    
-    $(location).attr('href',link);
-
-})
+  }
+  // let link = `https://api.whatsapp.com/send?phone=${fNum}`;
+  let formLink = window.location.href;
+  let link = `https://wa.me/${fNum}?text=Перейдите по этой ссылке ${formLink}`;
+  document.querySelector(".toWhatsApp").setAttribute("href", link);
+  document.querySelector(".toWhatsApp").click();
+});
