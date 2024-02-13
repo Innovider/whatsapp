b0f0b47ea4a922dc2f478b2bfc911688e1435f00
Innovider <42913925+Innovider@users.noreply.github.com>
2024-02-13 18:09:30 +0600

    correct code

--- style.js
+++ style.js
@@ -1,25 +1,25 @@
 document.getElementById("button").addEventListener("click", () => {
   let number = document.getElementById("number");
   let cNum = number.value;
-  let fNum = "";
-  for (let i = 0; i < cNum.length; i++) {
-    if (i === 0 && cNum[i] === "8") {
-      fNum = fNum + 7;
-    } else if (
-      cNum[i] === "+" ||
-      cNum[i] === "(" ||
-      cNum[i] === ")" ||
-      cNum[i] === "-" ||
-      cNum[i] === " "
-    ) {
-      fNum = fNum + "";
-    } else {
-      fNum = fNum + cNum[i];
+  cNum = cNum
+    .replaceAll(" ", "")
+    .replaceAll("(", "")
+    .replaceAll(")", "")
+    .replaceAll("-", "")
+    .replaceAll("+", "");
+  let fNum = [];
+  fNum.push(cNum);
+
+  if (fNum[0] !== "") {
+    if (fNum[0][0] === "8") {
+      fNum[0] = fNum[0].replace("8", "7");
     }
   }
-  // let link = `https://api.whatsapp.com/send?phone=${fNum}`;
+
+  // //let link = `https://api.whatsapp.com/send?phone=${fNum}`;
+
   let formLink = window.location.href;
   let link = `https://wa.me/${fNum}?text=Перейдите по этой ссылке ${formLink}`;
-  document.querySelector(".toWhatsApp").setAttribute("href", link);
-  document.querySelector(".toWhatsApp").click();
+  document.getElementById("link").setAttribute("href", link);
+  document.getElementById("link").click();
 });
